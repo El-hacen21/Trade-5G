0864977a65f399b01035c2b6d2dcd3db02480561
el-hacen.diallo <diallosegue@gmail.com>
2024-11-23 16:45:02 +0100

    improved the readme.md

--- protocol/src/applications/model/main.cc
+++ protocol/src/applications/model/main.cc
@@ -1,27 +1,70 @@
-/*
- * contains main
- * and code to compute some processes delay
- */
-
 #include "experiment.h"
+#include <map>
+#include <string>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 using namespace ns3;
 
 int main(int argc, char *argv[])
 {
+    // Map to store parameters and their default values
+    map<string, string> params = {
+        {"nb_robots", "4"},
+        {"scenario", "0"},
+        {"seed", "42"},
+        {"malicious", "0"},
+        {"commission", "5"},
+        {"duration", "100"}
+    };
 
-	if (argc == 7)
-	{
-		Experiment p(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
+    // Parse arguments in --parameter=value format
+    for (int i = 1; i < argc; ++i)
+    {
+        string arg = argv[i];
+        size_t eq_pos = arg.find('=');
+        if (eq_pos != string::npos && arg.substr(0, 2) == "--")
+        {
+            string key = arg.substr(2, eq_pos - 2); // Remove "--" and extract key
+            string value = arg.substr(eq_pos + 1);  // Extract value
+            if (params.find(key) != params.end())
+            {
+                params[key] = value; // Update the parameter value
+            }
+            else
+            {
+                cerr << "Unknown parameter: " << key << endl;
+                return 1;
+            }
+        }
+        else
+        {
+            cerr << "Invalid argument format: " << arg << endl;
+            return 1;
+        }
+    }
 
-		cout << "./simulator " << argv[1] << " " << argv[2] << " " << argv[3] << " " << argv[4] << " " << argv[5] << " " << argv[6] << " " << endl;
+    // Convert parameters to integers for the Experiment constructor
+    int nb_robots = stoi(params["nb_robots"]);
+    int scenario = stoi(params["scenario"]);
+    int seed = stoi(params["seed"]);
+    int malicious = stoi(params["malicious"]);
+    int commission = stoi(params["commission"]);
+    int duration = stoi(params["duration"]);
 
-		p.Trace();
-		p.Run();
-	}
-	else
-		cout << "Arguments !!!" << endl;
+    // Print parsed parameters for confirmation
+    cout << "Running simulator with the following parameters:" << endl;
+    for (const auto &param : params)
+    {
+        cout << "--" << param.first << "=" << param.second << endl;
+    }
 
-	return 0;
+    // Run the simulation
+    Experiment p(nb_robots, scenario, seed, malicious, commission, duration);
+
+    p.Trace();
+    p.Run();
+
+    return 0;
 }
